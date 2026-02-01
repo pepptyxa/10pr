8730ca527140b21c61973fe904d8a49f572c7237
pepptyxa <pheronerxe@gmail.com>
2026-02-01 12:07:52 +0500

    В коде страницы регистрации указала код, отображающий капчу

--- regin.php
+++ regin.php
@@ -44,6 +44,7 @@
 					<input name="_password" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
 					<div class = "sub-name">Повторите пароль:</div>
 					<input name="_passwordCopy" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
+					<center><div class="g-recaptcha" data-sitekey="6Lf17VwsAAAAAJT0rbX6LhuN30Ov3thb5S-dJAsM"></div></center>
 					
 					<a href="login.php">Вернуться</a>
 					<input type="button" class="button" value="Зайти" onclick="RegIn()" style="margin-top: 0px;"/>
